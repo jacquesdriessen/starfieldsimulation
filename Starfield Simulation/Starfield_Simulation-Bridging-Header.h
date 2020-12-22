b8d0c9de04dd8d8e1297477699b6e6a4fcd87483
Jacques Driessen <jacques.driessen@gmail.com>
2020-12-22 10:48:51 +0100

    Initial Commit

---
+++ Starfield Simulation/Starfield_Simulation-Bridging-Header.h
@@ -0,0 +1,12 @@
+//
+//  Starfield_Simulation-Bridging-Header.h
+//  Starfield Simulation
+//
+//  Created by Jacques Driessen on 22/12/2020.
+//
+
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+#import "ShaderTypes.h"
